9846d1166950b5a2c9d91924dac4224c989e558f
De-Dieu @hidden <mboulmbr@gmail.com>
2022-09-24 15:05:25 +0100

    unicafé, étape6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,29 +12,49 @@ const Statistics = ({good, neutral, bad, all}) => {
       const moyen = all / 2;
 
       return(
-      <>
-        <br/>
-        {/* <h1>Statistics</h1> */}
-        {/* <p>good  <strong> {good} </strong> </p>
-        <p>neutral <strong> {neutral} </strong> </p>
-        <p>bad <strong> {bad} </strong> </p>
-        <p>all <strong> {all} </strong> </p>
-        <p>average <strong> {moyen} </strong> </p> */}
-         <StatisticLine text="good" value ={good} />
-         <StatisticLine text="neutral" value ={neutral} />
-         <StatisticLine text="bad" value ={bad} />
-      </>
+       <>
+            <br/>
+            <h2>Statistics</h2>
+            <table>
+          <tbody>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutral} </td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{bad} </td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{all} </td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{moyen} </td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            {/* <td>{bad} </td> */}
+          </tr>
+          </tbody>
+        </table>
+       </>
       )
     }
 }
 
-const StatisticLine = ({text, value}) => {
-  return(
-    <>
-      <p> {text} <strong> {value} </strong> </p>
-    </>
-  )
-}
+// const StatisticLine = ({text, value}) => {
+//   return(
+//     <>
+//       <p> {text} <strong> {value} </strong> </p>
+//     </>
+//   )
+// }
 
 const ButtonVote = ( {handleClick, text} ) => {
   return(
@@ -55,16 +75,16 @@ const App = () => {
 
   let all =  good + neutral + bad;
 
-  const handleClick = () => {
-    console.log("hello");
-  }
+  // const handleClick = () => {
+  //   console.log("hello");
+  // }
 
   return (
     <div>
         <h1>give feedback</h1>
         <ButtonVote  handleClick={() => setGood(good + 1)} text="good" />
         <ButtonVote  handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-        <ButtonVote  handleClick={() => setBad(bad + 1)} text="bad" />
+        <ButtonVote  handleClick={() => setBad(bad + 1)} text="bad" /> <br/><br/>
         <Statistics all={all}  good={good} neutral={neutral} bad={bad} />
     </div>
   )
