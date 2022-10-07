b7e5b5bd8948679ece8a41eef98ee029f0c73d6f
De-Dieu @hidden <mboulmbr@gmail.com>
2022-10-07 03:20:37 +0100

    labs suite note

--- part2/labs/src/App.js
+++ part2/labs/src/App.js
@@ -1,25 +1,52 @@
+import React, {useState} from "react";
 import Note from "./components/Notes";
 
 const App = ({notes}) => {
   // const { notes } = props
+  const [note, setNotes] = useState(notes);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true)
 
-  // const lastElement = notes.at(-1);
+  const handlerSubmit = (event) => {
+      event.preventDefault();
+       console.log("newNote: ",newNote);
+      const oneNote = {
+        id: Math.random(),
+        content: newNote,
+        date: new Date(),
+        important: true
+      }
+       setNotes([...note, oneNote])
+  }
+
+  const notesToShow = showAll ? note : note.filter(note => note.important === true)
 
   return (
     <div>
-      <h1>Notes</h1>
+     <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {/* <li>{lastElement.content}</li> */}
         {
-          notes.map((item, index) => {
+          notesToShow.map((item, index) => {
               return(
-                <Note key={item.id} note={item} />
+                <Note key={index} note={item} />
+                // <p key={index}> { item.content} </p>
               )
           })
         }
        
         
       </ul>
+
+        <form onSubmit={handlerSubmit} >
+            <input  onChange={(e) => setNewNote(e.target.value)}  value={newNote}/>
+            <button  onClick={handlerSubmit} type="submit">save</button>
+        </form>
+
     </div>
   )
 }
--- part2/labs/src/components/Notes/index.js
+++ part2/labs/src/components/Notes/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Note = ({note}) => {
   return (
         <li>
-            <span>{note.date} </span>:  
-            {note.content}
+            {/* <span>{note.date} </span>:   */}
+            {note.content} <button>{note.important ? "Important" : "non important"}</button>
         </li>
   )
 }
