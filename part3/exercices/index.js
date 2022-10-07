33c4d2ce44b44cf46a79b1b8c391cb354773fd88
De-Dieu @hidden <mboulmbr@gmail.com>
2022-10-07 14:55:41 +0100

    Backend du répertoire téléphonique étape 6

--- part3/exercices/index.js
+++ part3/exercices/index.js
@@ -69,6 +69,43 @@ app.delete('/api/persons/:id', (req, res) => {
 });
 
 
+app.post('/api/persons', (req, res) => {
+        const element = req.body;
+
+        const newElement = { 
+            "id": Math.floor(Math.random() * 100),
+            "name": element.name, 
+            "number": element.number
+          }
+
+          let newTabs = data;
+
+
+        if (element.name && element.number) {
+          const tab =  newTabs.filter( item => item.name === element.name);
+            console.log('tab: ', tab);
+                    if(tab.length > 0){
+                        res.status(401).json({
+                            "error": "the name must be unique"
+                        })
+                    }else{
+                         newTabs = [...newTabs, newElement];
+                        res.status(200).json({
+                            "message": "element was added successful",
+                            "data": newTabs
+                        })
+                    }
+               
+        }else {
+            res.status(404).json({
+                "error": "le name et number are required"
+            })
+        }
+
+
+});
+
+
  app.listen(3001, () => {
     console.log("the server starting on port"+ 3001);
  });
